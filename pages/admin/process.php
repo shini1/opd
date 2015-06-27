f2b0e42e05dea1fa689b2d169f5235e85304cdcf
Manu K <manuknarayanan@gmail.com>
2015-06-27 23:35:36 +0530

    fix sql and password encrypt

--- pages/admin/process.php
+++ pages/admin/process.php
@@ -1,14 +1,16 @@
 <?php 
-echo $_POST["first_name"]."</br>";
-echo $_POST["last_name"]."</br>";
-echo $_POST["user_id"]."</br>";
-echo $_POST["password"]."</br>";
+$fname= $_POST["first_name"];
+$lname= $_POST["last_name"];
+$username= $_POST["user_id"];
+$pass=$_POST["password"];
+//for password encryption
+$encrypted_password=md5($pass);
 $link = mysqli_connect("localhost", "root", "", "opd");
 // Check connection
 if($link === false){
  die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$sql="INSERT INTO users (username, password, fname,lname )VALUES($_POST["user_id"],$_POST["password"],$_POST["first_name"],$_POST["last_name"])";
+$sql="INSERT INTO users ( username , password, fname, lname )VALUES( '$username','$encrypted_password','$fname','$lname')";
         if(mysqli_query($link, $sql)){
  echo "Records added successfully.";
 } else{
