84334c5edc5e3d6fe796b41bb77477ee4b393b2c
shini1 <shini94.sm@gmail.com>
2015-06-27 22:50:09 +0530

    sql

--- pages/admin/process.php
+++ pages/admin/process.php
@@ -3,3 +3,13 @@ echo $_POST["first_name"]."</br>";
 echo $_POST["last_name"]."</br>";
 echo $_POST["user_id"]."</br>";
 echo $_POST["password"]."</br>";
+$link = mysqli_connect("localhost", "root", "", "opd");
+// Check connection
+if($link === false){
+ die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+        if(mysqli_query($link, $sql)){
+ echo "Records added successfully.";
+} else{
+ echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
