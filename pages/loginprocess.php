d4a35a09a965307ac396720d1850268a00816bb1
Manu K <manuknarayanan@gmail.com>
2015-06-28 22:50:33 +0530

    login process fix

--- pages/loginprocess.php
+++ pages/loginprocess.php
@@ -1,16 +1,18 @@
-<?php 
-class login{
-    public $errors = array(); 
- public $messages = array();
-    
+<?php
+
+class login {
+
+    public $errors = array();
+    public $messages = array();
+
+    public function __construct() {
+
+        session_start();
+        if (isset($_GET["logout"])) {
+            $this->doLogout();
+        } elseif (isset($_POST["login"])) {
+            $this->dologinWithPostData();
+        }
+    }
+
 }
-public function __construct() {
- 
- session_start(); 
- if (isset($_GET["logout"])) {
- $this->doLogout();
- } 
- elseif (isset($_POST["login"])) {
- $this->dologinWithPostData();
- }
- }
