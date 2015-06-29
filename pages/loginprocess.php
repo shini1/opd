e799e1176d00c8659fbb432c0d0bcea083fa4448
shini1 <shini94.sm@gmail.com>
2015-06-29 22:37:32 +0530

    loginprocess

--- pages/loginprocess.php
+++ pages/loginprocess.php
@@ -14,5 +14,31 @@ class login {
             $this->dologinWithPostData();
         }
     }
+private function dologinWithPostData(){
+
+
+    
+    $username = $_POST["user_id"];
+    $pass = $_POST["password"];
+//for password encryption
+    $encrypted_password = md5($pass);
+    require('config/mysql.php');
+    //username already exist or not
+     $sql = "SELECT * FROM users WHERE username = '$username and password='$password';";
+    if ($result = mysqli_query($link, $sql)) {
+        $num_rows = mysqli_num_rows($result); // no of rows in the result
+
+
+        if ($num_rows == 1) {
+            echo "USER Verified";
+        } else {
+            echo "USER not Verified";
+        }
+            
+        }
+        else{
+            echo "ERROR: Could not able to execute";
+        }
+}
 
 }
