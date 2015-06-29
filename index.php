dda6d07204995336bfcc61504028751d6f15eb38
shini1 <shini94.sm@gmail.com>
2015-06-30 00:45:12 +0530

    index

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-SHINI
-
+<?php
+require_once("pages/loginprocess.php");
+$login = new Login();
+if ($login->isUserLoggedIn() == true) {
+ echo "logged in";
+ 
+ 
+ }
+ else{
+ include("pages/login.php");
+ 
+ }
