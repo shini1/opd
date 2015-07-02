f1acc4b74a62cb1cec9bcc75668942f11872e09e
Manu K <manuknarayanan@gmail.com>
2015-07-02 19:08:11 +0530

    fix

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     echo "logged in";
     if ($login->usertype() == "admin") {
-        include(pages / admin / home . php);
-    } else if ($login->usertype() == "receptionist") {
-        include(pages / receptionist / home . php);
-    } else if ($login->usertype() == "doctor") {
-        include(pages / doctor / home . php);
-    } else if ($login->usertype() == "patient") {
-        include(pages / patient / home . php);
+        include(pages/admin/home.php);
+    } else if ($login->usertype()== "receptionist") {
+        include(pages/receptionist/home.php);
+    } else if ($login->usertype()== "doctor") {
+        include(pages/doctor/home.php);
+    } else if ($login->usertype()== "patient") {
+        include(pages/patient/home.php);
     }
 } else {
     include("pages/login.php");
