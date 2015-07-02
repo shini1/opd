f575271d6ef436dd56a5302129dde3d4de5fb1e4
Manu K <manuknarayanan@gmail.com>
2015-07-02 18:59:15 +0530

    fix

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
+
 require_once("pages/loginprocess.php");
 $login = new Login();
 if ($login->isUserLoggedIn() == true) {
- echo "logged in";
- {
-	 else if ($login->usertype() == "admin") {
-     include(pages/admin/home.php);}
-
-      else if ($login->usertype() == "receptionist") {
-      include(pages/receptionist/home.php);}
-
-      else if ($login->usertype() == "doctor") {
-      include(pages/doctor/home.php);}
-       else if ($login->usertype() == "patient") {
-       include(pages/patient/home.php);}
- }
-
- else{
- include("pages/login.php");
- 
- }
+    echo "logged in";
+    if ($login->usertype() == "admin") {
+        include(pages / admin / home . php);
+    } else if ($login->usertype() == "receptionist") {
+        include(pages / receptionist / home . php);
+    } else if ($login->usertype() == "doctor") {
+        include(pages / doctor / home . php);
+    } else if ($login->usertype() == "patient") {
+        include(pages / patient / home . php);
+    }
+} else {
+    include("pages/login.php");
+}
